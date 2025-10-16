8bea097b1fa5f33ae79ca5b360426aeb093668a3
pmg532690 <pmg532690@sookmyung.ac.kr>
2025-10-16 15:35:13 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int inc(int counter);
 
+
 int main(int argc, char *argv[]) {
+	
 	
 	int i = 10;
 	printf("함수 호출 전 i=%d\n", i);
-	inc(i);
+	i = inc(i);
 	printf("함수 호출 후i=%d\n", i);
 
 	return 0;
